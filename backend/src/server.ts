f7fea26be8b295c9f238eb6d01b66e9ab00b0f7c
mthanuj <mullagurithanuj0@gmail.com>
2025-06-01 07:15:16 +0530

    flow for env configs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,6 @@
-console.log('hello');
+import app from './app';
+import config from './config/config';
+
+app.listen(config.PORT, config.HOST, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
