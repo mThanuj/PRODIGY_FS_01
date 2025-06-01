c363c36525f3237a31fc52adbca78918cd93d550
mthanuj <mullagurithanuj0@gmail.com>
2025-06-01 15:36:41 +0530

    add task model

---
+++ backend/src/models/task.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model, models, Document } from 'mongoose';
+
+interface ITask extends Document {
+  title: string;
+  description: string;
+  isDone: boolean;
+  user: Schema.Types.ObjectId;
+}
+
+const taskSchema = new Schema<ITask>({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+  },
+  isDone: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Task = models.Task || model<ITask>('Task', taskSchema);
+
+export default Task;
