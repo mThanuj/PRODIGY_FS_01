c6ab79669858cd11fa5c7f6733f158e3534a354a
mthanuj <mullagurithanuj0@gmail.com>
2025-06-01 07:30:10 +0530

    mongodb connection

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,11 +3,15 @@ import 'dotenv/config';
 interface Config {
   PORT: number;
   HOST: string;
+  MONGODB_URI: string;
 }
 
 const config: Config = {
   PORT: Number(process.env.PORT) || 3000,
   HOST: process.env.HOST || 'localhost',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
 };
+
+console.log(config);
 
 export default config;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import config from './config/config';
+import connectDB from './config/db';
 
-app.listen(config.PORT, config.HOST, () => {
+app.listen(config.PORT, config.HOST, async () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  await connectDB();
 });
