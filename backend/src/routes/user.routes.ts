5c4f2ef241462392866db3b1a5cbfce6b87f3b95
mthanuj <mullagurithanuj0@gmail.com>
2025-06-01 16:14:31 +0530

    make sure user is admin to fetch all users

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { authenticateToken } from '../middlewares/auth.middleware';
+import { authenticateToken, isAdmin } from '../middlewares/auth.middleware';
 import { getAllUsers } from '../controllers/user.controller';
 
 const router = express.Router();
 
 router.use(authenticateToken);
 
-router.get('/', getAllUsers);
+router.get('/', isAdmin, getAllUsers);
 
 export default router;
