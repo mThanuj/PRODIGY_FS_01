43fb28cf026537e85344728dd39d7a9e269c6b47
mthanuj <mullagurithanuj0@gmail.com>
2025-06-01 07:22:09 +0530

    routing flow for auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+import authRoutes from './routes/auth.routes';
+
+app.use('/api/v1/auth', authRoutes);
 
 export default app;
