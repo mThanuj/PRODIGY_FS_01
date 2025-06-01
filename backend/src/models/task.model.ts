869531f2c8b087bfce331640aed991bf40705071
mthanuj <mullagurithanuj0@gmail.com>
2025-06-01 16:31:58 +0530

    add timestamps and sort the tasks with latest first

--- backend/src/models/task.model.ts
+++ backend/src/models/task.model.ts
@@ -7,25 +7,30 @@ export interface ITask extends Document {
   user: Schema.Types.ObjectId;
 }
 
-const taskSchema = new Schema<ITask>({
-  title: {
-    type: String,
-    required: [true, 'Title is required'],
+const taskSchema = new Schema<ITask>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+    isDone: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: [true, 'Description is required'],
+  {
+    timestamps: true,
   },
-  isDone: {
-    type: Boolean,
-    default: false,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-});
+);
 
 const Task = models.Task || model<ITask>('Task', taskSchema);
 
